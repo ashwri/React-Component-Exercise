7fd2610198e14cbbebee52721395f933f44dd19d
ashwri <ashley.gail.wright@gmail.com>
2019-12-28 15:38:01 -0800

    Todo 2 and 3 Completed: Display the input and output component via the app component

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p> I'm paragraph 1 </p>
+            <p> I'm paragraph 2 </p> 
+        </div>
+    ) 
+}
+
+export default userOutput;
