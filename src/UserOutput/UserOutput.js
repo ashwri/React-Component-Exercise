992aa9af41c566f3a216eb450adfb5f301281af2
ashwri <ashley.gail.wright@gmail.com>
2019-12-30 14:53:24 -0800

    Added a simple style change to the output

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './UserOutput.css'
 
 const userOutput = (props) => {
     return (
-        <div>
+        <div className = 'user-output'>
             <p> This example demonstrates <b> {props.type} </b>  passed output to the component. </p> 
             <p> Hi! I'm {props.username} </p>
         </div>
